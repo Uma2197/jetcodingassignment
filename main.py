28a36eb2fab319b9cdba378d59e61f242de0f667
Uma2197 <124685592+Uma2197@users.noreply.github.com>
2024-04-21 10:23:07 +0200

    Restaurant name Update main.py
    We are creating a function called display_restaurant_info to display the required data points and initially display the restaurant's name.

--- main.py
+++ main.py
@@ -8,4 +8,15 @@ response = requests.get(url=api_endpoint, headers=headers)
 response.raise_for_status()
 api_data = response.json()
 restaurant_data = api_data["restaurants"]
-print(restaurant_data)
+# print(restaurant_data)
+
+
+def display_restaurant_info(data):
+    for restaurant in data:
+        name = restaurant['name']
+        
+        print(f"Name: {name}")
+
+
+display_restaurant_info(restaurant_data)
+
