53b8e5b1ab1f1d2e364bacd64c84a77da873a00b
Uma2197 <124685592+Uma2197@users.noreply.github.com>
2024-04-21 10:31:55 +0200

    Name and Cuisines Update main.py
    Now, we are adding in the cuisines to display. We're making use of the join() method for a better view of the results.

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ restaurant_data = api_data["restaurants"]
 def display_restaurant_info(data):
     for restaurant in data:
         name = restaurant['name']
-        
-        print(f"Name: {name}")
+        cuisines = []
+        for cuisine in restaurant["cuisines"]:
+            cuisines.append(cuisine["name"])
+        cuisine = ",".join(cuisines)
+
+        print(f"Name: {name}\nCuisines: {cuisine}")
 
 
 display_restaurant_info(restaurant_data)
